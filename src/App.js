b85aa695b7270686853475808a56444432ed1478
Kseniya <loian4ikg@gmail.com>
2021-02-08 16:34:53 +0100

    update my reactwebapp

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
 
    if(city) {
    const api_url = await
-     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
     const data = await api_url.json();
 
     var sunset = data.sys.sunset;
